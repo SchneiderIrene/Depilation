60abfa4c5bdecfc876e60ed0098af41d3b11147a
Irene Schneider <irene.schneider.web@gmail.com>
2025-09-27 15:11:23 +0200

    path certificate

--- src/data/certificate.ts
+++ src/data/certificate.ts
@@ -1,14 +1,14 @@
 export const certificates = [
-  { id: 1, type: "portrait", src: "/src/assets/certificate1.png" },
-  { id: 2, type: "portrait", src: "/src/assets/certificate2.png" },
-  { id: 3, type: "portrait", src: "/src/assets/certificate3.png" },
-  { id: 4, type: "portrait", src: "/src/assets/certificate4.png" },
-  { id: 5, type: "portrait", src: "/src/assets/certificate5.png" },
-  { id: 6, type: "landscape", src: "/src/assets/certificate6.png" },
-  { id: 7, type: "landscape", src: "/src/assets/certificate7.png" },
-  { id: 8, type: "landscape", src: "/src/assets/certificate8.png" },
-  { id: 9, type: "landscape", src: "/src/assets/certificate9.png" },
-  { id: 10, type: "landscape", src: "/src/assets/certificate10.png" },
-  { id: 11, type: "landscape", src: "/src/assets/certificate11.png" },
-  { id: 12, type: "landscape", src: "/src/assets/certificate12.png" }
+  { id: 1, type: "portrait", src: "/assets/certificate1.png" },
+  { id: 2, type: "portrait", src: "/assets/certificate2.png" },
+  { id: 3, type: "portrait", src: "/assets/certificate3.png" },
+  { id: 4, type: "portrait", src: "/assets/certificate4.png" },
+  { id: 5, type: "portrait", src: "/assets/certificate5.png" },
+  { id: 6, type: "landscape", src: "/assets/certificate6.png" },
+  { id: 7, type: "landscape", src: "/assets/certificate7.png" },
+  { id: 8, type: "landscape", src: "/assets/certificate8.png" },
+  { id: 9, type: "landscape", src: "/assets/certificate9.png" },
+  { id: 10, type: "landscape", src: "/assets/certificate10.png" },
+  { id: 11, type: "landscape", src: "/assets/certificate11.png" },
+  { id: 12, type: "landscape", src: "/assets/certificate12.png" }
 ]
